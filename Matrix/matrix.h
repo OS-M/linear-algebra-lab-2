b208152b907eabf6edd93c67aaa63f022ab6f69e
quackable <manalog44@gmail.com>
2021-11-25 15:52:25 +0300

    Implemented matrix #2

--- Matrix/matrix.h
+++ Matrix/matrix.h
@@ -1,28 +1,40 @@
 #pragma once
 
 #include <memory>
+#include <random>
+#include <sstream>
+#include <iomanip>
+
+template<class T, class U>
+void AssertEqualSizes(const T& a, const U& b);
+
+template<class T, class U>
+std::string PairToString(const std::pair<T, U>& pair) {
+  return "(" + std::to_string(pair.first) + "; " +
+      std::to_string(pair.second) + ")";
+}
 
 template<class T>
 class Matrix {
  public:
   Matrix();
   Matrix(int n, int m);
+  Matrix(const Matrix<T>& other);
+  Matrix(Matrix<T>&& other) noexcept;
   explicit Matrix(int n);
 
   std::pair<int, int> Size() const;
+
+  bool IsSquare() const;
 
   int Rows() const;
   int Cols() const;
 
   Matrix<T> Transposed();
 
-  static Matrix<T> Ones(int n, int m);
+  static Matrix<T> Ones(int n);
   static Matrix<T> Zeros(int n, int m);
-  Matrix<T> Matrix<T>::Random(int n,
-                              int m,
-                              T min,
-                              T max,
-                              int seed = time(nullptr));
+  static Matrix<T> Random(int n, int m, T min, T max, int seed = time(nullptr));
 
   std::string ToWolframString() const;
 
@@ -39,6 +51,7 @@ class Matrix {
   const Matrix<T> SubMatrix(int i, int j, int n, int m) const;
   Matrix<T> SubMatrix(int i, int j, int n, int m);
 
+  bool IsVector() const;
   bool IsRowVector() const;
   bool IsColVector() const;
 
@@ -47,42 +60,56 @@ class Matrix {
   const Matrix<T> Col(int j) const;
   Matrix<T> Col(int j);
 
-  friend Matrix<T>& operator=(Matrix<T>& a, const Matrix<T>& b);
-  friend Matrix<T>& operator=(Matrix<T>& a, Matrix<T>&& b);
+  Matrix<T>& operator=(const Matrix<T>& b);
+  Matrix<T>& operator=(Matrix<T>&& b);
 
-  friend Matrix<T> operator+(const Matrix<T>& a, const Matrix<T>& b);
-  friend Matrix<T> operator-(const Matrix<T>& a, const Matrix<T>& b);
-  friend Matrix<T> operator*(const Matrix<T>& a, const Matrix<T>& b);
-  friend Matrix<T> operator/(const Matrix<T>& a, const Matrix<T>& b);
-  friend T ScalarProduct(const Matrix<T>& a, const Matrix<T>& b);
-  friend Matrix<T> operator*(const Matrix<T>& a, T b);
-  friend Matrix<T> operator/(const Matrix<T>& a, T b);
+  template<class U>
+  friend Matrix<U> operator+(const Matrix<U>& a, const Matrix<U>& b);
+  template<class U>
+  friend Matrix<U> operator-(const Matrix<U>& a, const Matrix<U>& b);
+  template<class U>
+  friend Matrix<U> operator*(const Matrix<U>& a, const Matrix<U>& b);
+  T ScalarProduct(const Matrix<T>& b) const;
+  template<class U, class W>
+  friend Matrix<U> operator*(const Matrix<U>& a, W b);
+  template<class U, class W>
+  friend Matrix<U> operator/(const Matrix<U>& a, W b);
 
   Matrix<T>& operator+=(const Matrix<T>& a);
   Matrix<T>& operator-=(const Matrix<T>& a);
   Matrix<T>& operator*=(const Matrix<T>& a);
-  Matrix<T>& operator/=(const Matrix<T>& a);
-  Matrix<T> operator*=(T b);
-  Matrix<T> operator/=(T b);
+  template<class U>
+  Matrix<T>& operator*=(U b);
+  template<class U>
+  Matrix<T>& operator/=(U b);
 
-  friend operator==(const Matrix<T>& a, const Matrix<T>& b);
-  friend operator!=(const Matrix<T>& a, const Matrix<T>& b);
+  template<class U>
+  friend bool operator==(const Matrix<U>& a, const Matrix<U>& b);
+  template<class U>
+  friend bool operator!=(const Matrix<U>& a, const Matrix<U>& b);
 
+  static T eps;
  private:
   std::shared_ptr<T[]> data_;
   int data_rows_;
   int data_cols_;
   int cols_;
   int rows_;
+  int offset_i_;
+  int offset_j_;
 
   bool IsSubMatrix() const;
 };
+
+template<class T>
+T Matrix<T>::eps = T();
 
 template<class T>
 Matrix<T>::Matrix() : Matrix<T>(0, 0) {}
 
 template<class T>
 Matrix<T>::Matrix(int n, int m) : data_(new T[n * m]) {
+  offset_i_ = offset_j_ = 0;
   data_rows_ = rows_ = n;
   data_cols_ = cols_ = m;
   for (int i = 0; i < this->Rows(); i++) {
@@ -112,13 +139,19 @@ int Matrix<T>::Cols() const {
 
 template<class T>
 Matrix<T> Matrix<T>::Transposed() {
-  return Matrix<T>();
+  Matrix<T> a(this->Cols(), this->Rows());
+  for (int i = 0; i < this->Cols(); i++) {
+    for (int j = 0; j < this->Rows(); j++) {
+      a.At(i, j) = this->At(j, i);
+    }
+  }
+  return a;
 }
 
 template<class T>
-Matrix<T> Matrix<T>::Ones(int n, int m) {
-  Matrix<T> a(n, m);
-  for (int i = 0; i < std::min(n, m); i++) {
+Matrix<T> Matrix<T>::Ones(int n) {
+  Matrix<T> a(n);
+  for (int i = 0; i < n; i++) {
     a(i, i) = 1;
   }
   return a;
@@ -134,8 +167,8 @@ Matrix<T> Matrix<T>::Random(int n, int m, T min, T max, int seed) {
   Matrix<T> a(n, m);
   std::mt19937 gen(seed);
   std::uniform_real_distribution<T> dist(min, max);
-  for (int i = 0; i < this->Rows(); i++) {
-    for (int j = 0; j < this->Cols(); j++) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
       a(i, j) = dist(gen);
     }
   }
@@ -185,80 +218,273 @@ const T& Matrix<T>::operator()(int i, int j) const {
 
 template<class T>
 const Matrix<T> Matrix<T>::SubMatrix(int i, int j, int n, int m) const {
-  return Matrix<T>();
+  return const_cast<Matrix<T>*>(this)->SubMatrix(i, j, n, m);
 }
+
 template<class T>
 Matrix<T> Matrix<T>::SubMatrix(int i, int j, int n, int m) {
-  return Matrix<T>();
+  Matrix<T> a;
+  a.data_ = this->data_;
+  a.rows_ = n;
+  a.cols_ = m;
+  a.offset_i_ = this->offset_i_ + i;
+  a.offset_j_ = this->offset_j_ + j;
+  a.data_cols_ = this->data_cols_;
+  a.data_rows_ = this->data_rows_;
+  return a;
 }
+
 template<class T>
 const Matrix<T> Matrix<T>::Row(int i) const {
-  return Matrix<T>();
+  return const_cast<Matrix<T>*>(this)->Row(i);
 }
+
 template<class T>
 Matrix<T> Matrix<T>::Row(int i) {
-  return Matrix<T>();
+  return this->SubMatrix(i, 0, 1, this->Cols());
 }
+
 template<class T>
 const Matrix<T> Matrix<T>::Col(int j) const {
-  return Matrix<T>();
+  return const_cast<Matrix<T>*>(this)->Col(j);
 }
+
 template<class T>
 Matrix<T> Matrix<T>::Col(int j) {
-  return Matrix<T>();
+  return this->SubMatrix(0, j, this->Rows(), 1);
 }
+
 template<class T>
 Matrix<T>& Matrix<T>::operator+=(const Matrix<T>& a) {
-  return <#initializer#>;
+  AssertEqualSizes(*this, a);
+  for (int i = 0; i < a.Rows(); ++i) {
+    for (int j = 0; j < a.Cols(); ++j) {
+      this->At(i, j) += a.At(i, j);
+    }
+  }
+  return *this;
 }
+
 template<class T>
 Matrix<T>& Matrix<T>::operator-=(const Matrix<T>& a) {
-  return <#initializer#>;
+  AssertEqualSizes(*this, a);
+  for (int i = 0; i < a.Rows(); ++i) {
+    for (int j = 0; j < a.Cols(); ++j) {
+      this->At(i, j) -= a.At(i, j);
+    }
+  }
+  return *this;
 }
+
 template<class T>
 Matrix<T>& Matrix<T>::operator*=(const Matrix<T>& a) {
-  return <#initializer#>;
+  *this = *this * a;
 }
+
 template<class T>
-Matrix<T>& Matrix<T>::operator/=(const Matrix<T>& a) {
-  return <#initializer#>;
+template<class U>
+Matrix<T>& Matrix<T>::operator*=(U b) {
+  for (int i = 0; i < this->Rows(); ++i) {
+    for (int j = 0; j < this->Cols(); ++j) {
+      this->At(i, j) *= b;
+    }
+  }
+  return *this;
 }
+
 template<class T>
-Matrix<T> Matrix<T>::operator*=(T b) {
-  return Matrix<T>();
+template<class U>
+Matrix<T>& Matrix<T>::operator/=(U b) {
+  for (int i = 0; i < this->Rows(); ++i) {
+    for (int j = 0; j < this->Cols(); ++j) {
+      this->At(i, j) /= b;
+    }
+  }
+  return *this;
 }
-template<class T>
-Matrix<T> Matrix<T>::operator/=(T b) {
-  return Matrix<T>();
-}
+
 template<class T>
 bool Matrix<T>::IsSubMatrix() const {
-  return false;
+  return !(cols_ == data_cols_
+      && rows_ == data_rows_
+      && offset_i_ == 0
+      && offset_j_ == 0);
 }
 
 template<class T>
 bool Matrix<T>::IsRowVector() const {
-  return false;
+  return this->Rows() == 1;
 }
+
 template<class T>
 bool Matrix<T>::IsColVector() const {
-  return false;
+  return this->Cols() == 1;
 }
+
 template<class T>
 T& Matrix<T>::At(int i) {
-  return <#initializer#>;
+  if (this->IsRowVector()) {
+    return this->At(0, i);
+  } else if (this->IsColVector()) {
+    return this->At(i, 0);
+  } else {
+    throw std::runtime_error(
+        "trying to get value by single index in matrix of size "
+            + PairToString(this->Size()));
+  }
 }
+
 template<class T>
 const T& Matrix<T>::At(int i) const {
-  return <#initializer#>;
+  return const_cast<Matrix<T>*>(this)->At(i);
 }
+
 template<class T>
 T& Matrix<T>::At(int i, int j) {
-  return <#initializer#>;
+#ifndef DEBUG
+  if (i < 0 || j < 0 || i >= rows_ || j >= cols_) {
+    throw std::out_of_range(
+        "Indexes (" + std::to_string(i) + "; " + std::to_string(j)
+            + ") out of matrix size " + PairToString(this->Size()));
+  }
+#endif
+  return data_[(i + offset_i_) * data_cols_ + offset_j_ + j];
 }
+
 template<class T>
 const T& Matrix<T>::At(int i, int j) const {
-  return <#initializer#>;
+  return const_cast<Matrix<T>*>(this)->At(i, j);
+}
+
+template<class T>
+bool Matrix<T>::IsSquare() const {
+  return this->Cols() == this->Rows();
+}
+
+template<class T>
+bool Matrix<T>::IsVector() const {
+  return this->IsColVector() || this->IsRowVector();
+}
+
+template<class T>
+Matrix<T> operator+(const Matrix<T>& a, const Matrix<T>& b) {
+  AssertEqualSizes(a, b);
+  Matrix<T> res = a;
+  res += b;
+  return res;
+}
+
+template<class T>
+Matrix<T> operator-(const Matrix<T>& a, const Matrix<T>& b) {
+  AssertEqualSizes(a, b);
+  Matrix<T> res = a;
+  res -= b;
+  return res;
+}
+
+template<class T>
+Matrix<T> operator*(const Matrix<T>& lhs, const Matrix<T>& rhs) {
+  AssertEqualSizes(lhs, rhs);
+  Matrix<T> result(lhs.Rows(), rhs.Cols());
+  for (int i = 0; i < lhs.Rows(); i++) {
+    for (int k = 0; k < lhs.Cols(); k++) {
+      for (int j = 0; j < rhs.Cols(); j++) {
+        result.At(i, j) += lhs.At(i, k) * rhs.At(k, j);
+      }
+    }
+  }
+  return result;
+}
+
+template<class T, class U>
+Matrix<T> operator*(const Matrix<T>& lhs, U rhs) {
+  Matrix<T> result = lhs;
+  result *= rhs;
+  return result;
+}
+
+template<class T, class U>
+Matrix<T> operator/(const Matrix<T>& lhs, U rhs) {
+  Matrix<T> result = lhs;
+  result *= rhs;
+  return result;
+}
+
+template<class T>
+T Matrix<T>::ScalarProduct(const Matrix<T>& b) const {
+  if (!this->IsVector() || !b.IsVector()) {
+    throw std::runtime_error("Matrices of sizes " + PairToString(this->Size()) +
+        " and " + PairToString(b.Size()) + " are not both vectors");
+  }
+  T ans = T();
+  for (int i = 0; i < std::max(this->Cols(), this->Rows()); i++) {
+    ans += this->At(i) * b.At(i);
+  }
+  return ans;
+}
+
+template<class T>
+bool operator==(const Matrix<T>& a, const Matrix<T>& b) {
+  return !(a != b);
+}
+
+template<class T>
+bool operator!=(const Matrix<T>& a, const Matrix<T>& b) {
+  AssertEqualSizes(a, b);
+  for (int i = 0; i < a.Rows(); i++) {
+    for (int j = 0; j < a.Cols(); j++) {
+      if (a(i, j) != a(i, j) || b(i, j) != b(i, j) ||  // check for NaNs
+          std::abs(a(i, j) - b(i, j)) > Matrix<T>::eps) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+template<class T>
+Matrix<T>::Matrix(const Matrix<T>& other) :
+    Matrix<T>(other.Rows(), other.Cols()) {
+  for (int i = 0; i < this->Rows(); i++) {
+    for (int j = 0; j < this->Cols(); j++) {
+      this->At(i, j) = other.At(i, j);
+    }
+  }
+}
+
+template<class T>
+Matrix<T>::Matrix(Matrix<T>&& other) noexcept : Matrix() {
+  *this = other;
+}
+
+template<class T>
+Matrix<T>& Matrix<T>::operator=(const Matrix<T>& b) {
+  for (int i = 0; i < this->Rows(); i++) {
+    for (int j = 0; j < this->Cols(); j++) {
+      this->At(i, j) = b.At(i, j);
+    }
+  }
+  return *this;
+}
+
+template<class T>
+Matrix<T>& Matrix<T>::operator=(Matrix<T>&& b) {
+  std::swap(data_, b.data_);
+  std::swap(data_rows_, data_rows_);
+  std::swap(data_cols_, data_cols_);
+  std::swap(cols_, cols_);
+  std::swap(rows_, rows_);
+  std::swap(offset_i_, offset_i_);
+  std::swap(offset_j_, offset_j_);
+  return *this;
+}
+
+template<class T, class U>
+void AssertEqualSizes(const T& a, const U& b) {
+  if (a.Size() != b.Size()) {
+    throw std::invalid_argument("Wrong matrix sizes: " +
+        PairToString(a.Size()) + " " + PairToString(b.Size()));
+  }
 }
 
 template<class U>
@@ -291,3 +517,5 @@ std::ostream& operator<<(std::ostream& stream,
   stream << "]\n";
   return stream;
 }
+
+typedef Matrix<double> DMatrix;
--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "Matrix/matrix.h"
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+  Matrix<double>::eps = 1e-6;
+
+  auto a = DMatrix::Ones(3);
+  a(1, 2) = 1;
+  auto b = a * a.Transposed() * 3;
+  std::cout << a << b;
+  b.Col(1) *= 2;
+  b.Row(0) *= 2;
+  auto c = b.SubMatrix(2, 1, 1, 2).Transposed();
 }
